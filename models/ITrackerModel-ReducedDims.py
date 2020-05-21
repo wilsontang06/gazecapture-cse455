2a6ec5f36375aec62890aae55a1773c63a1d9084
William Kim <williamkim12345@hotmail.com>
2020-05-20 21:52:59 -0700

    model with reduced dimensions and layers as described in doc

---
+++ models/ITrackerModel-ReducedDims.py
@@ -0,0 +1,143 @@
+import argparse
+import os
+import shutil
+import time, math
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import numpy as np
+import torch.utils.model_zoo as model_zoo
+from torch.autograd.variable import Variable
+
+'''
+Pytorch model for the iTracker.
+
+Author: Petr Kellnhofer ( pkel_lnho (at) gmai_l.com // remove underscores and spaces), 2018. 
+
+Website: http://gazecapture.csail.mit.edu/
+
+Cite:
+
+Eye Tracking for Everyone
+K.Krafka*, A. Khosla*, P. Kellnhofer, H. Kannan, S. Bhandarkar, W. Matusik and A. Torralba
+IEEE Conference on Computer Vision and Pattern Recognition (CVPR), 2016
+
+@inproceedings{cvpr2016_gazecapture,
+Author = {Kyle Krafka and Aditya Khosla and Petr Kellnhofer and Harini Kannan and Suchendra Bhandarkar and Wojciech Matusik and Antonio Torralba},
+Title = {Eye Tracking for Everyone},
+Year = {2016},
+Booktitle = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)}
+}
+
+'''
+
+
+class ItrackerImageModel(nn.Module):
+    # Used for both eyes (with shared weights) and the face (with unqiue weights)
+    def __init__(self):
+        super(ItrackerImageModel, self).__init__()
+        self.features = nn.Sequential(
+            # nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=3, stride=2),
+            # # nn.CrossMapLRN2d(size=5, alpha=0.0001, beta=0.75, k=1.0),
+            # nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=2),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=3, stride=2),
+            # # nn.CrossMapLRN2d(size=5, alpha=0.0001, beta=0.75, k=1.0),
+            # nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(384, 64, kernel_size=1, stride=1, padding=0),
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(3, 32, kernel_size=11, stride=4, padding=0),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class FaceImageModel(nn.Module):
+    
+    def __init__(self):
+        super(FaceImageModel, self).__init__()
+        self.conv = ItrackerImageModel()
+        self.fc = nn.Sequential(
+            nn.Linear(12*12*64, 64),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x
+
+class FaceGridModel(nn.Module):
+    # Model for the face grid pathway
+    def __init__(self, gridSize = 25):
+        super(FaceGridModel, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(gridSize * gridSize, 128),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+
+class ITrackerModel(nn.Module):
+
+
+    def __init__(self):
+        super(ITrackerModel, self).__init__()
+        self.eyeModel = ItrackerImageModel()
+        self.faceModel = FaceImageModel()
+        self.gridModel = FaceGridModel()
+        # Joining both eyes
+        self.eyesFC = nn.Sequential(
+            nn.Linear(2*12*12*64, 64),
+            nn.ReLU(inplace=True),
+        )
+        # Joining everything
+        self.fc = nn.Sequential(
+            nn.Linear(64+64+128, 2),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, faces, eyesLeft, eyesRight, faceGrids):
+        # print("eyesLeft", eyesLeft.size())
+        # Eye nets
+        xEyeL = self.eyeModel(eyesLeft) # batch size X 9216
+        xEyeR = self.eyeModel(eyesRight) # batch size X 9216
+        # Cat and FC
+        xEyes = torch.cat((xEyeL, xEyeR), 1)
+        xEyes = self.eyesFC(xEyes) # batch size X 128
+        #print("xEyeL/R", xEyeL.size(), xEyeR.size(), xEyes.size())
+        # xEyes = torch.zeros((eyesLeft.size()[0], 128)).cuda()
+
+        # Face net
+        xFace = self.faceModel(faces)
+        #print(xFace.size())
+        xGrid = self.gridModel(faceGrids)
+        #print(xGrid.size())
+
+        # Cat all
+        x = torch.cat((xEyes, xFace, xGrid), 1)
+        x = self.fc(x)
+        
+        return x
